8912939f78ce7ff84cb37de738bd1799a3fae3bd
toy-php <seymus007@gmail.com>
2018-12-10 22:47:06 +0300

    Добавлен метод подписки на единичное выполнение функции наблюдателя

--- src/core/EventsTrait.php
+++ src/core/EventsTrait.php
@@ -22,6 +22,20 @@ trait EventsTrait
     }
 
     /**
+     * Подписка на единичное исполнение функции слушателя
+     * @param string $event
+     * @param callable $listener
+     */
+    public function once(string $event, callable $listener)
+    {
+        $onceListener = function ($context) use (&$onceListener, $event, $listener){
+            $listener($context);
+            $this->off($event, $onceListener);
+        };
+        $this->on($event, $onceListener);
+    }
+
+    /**
      * Отписка от событий
      * @param string $event
      * @param callable $listener
